a08d367355079ed66d5c5f7f99453fdec653d19a
mark-williams <markwill@hotmail.co.uk>
2018-02-14 15:58:18 +0000

    Reorg: now uses react-scripts (like create-react-app) rather than hand-rolled server and gulp files.

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Root from './posts/root';
 
-ReactDom.render(<Root />, document.getElementById('app'));
+ReactDom.render(<Root />, document.getElementById('root'));
 
