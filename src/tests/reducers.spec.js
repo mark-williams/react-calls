7d646b36dd30c6960cc5a620c5743063cf0f8ae5
mark-williams <markwill@hotmail.co.uk>
2018-02-15 10:19:14 +0000

    refactor: removed use of 'var'

--- src/tests/reducers.spec.js
+++ src/tests/reducers.spec.js
@@ -24,60 +24,60 @@ describe('Reducer tests', () => {
 
   describe('postsReducer', () => {
     it('should set posts to empty for an unsuported action', () => {
-      var newState = postsReducer(undefined, { type: 'INVALID', data: null });
+      const newState = postsReducer(undefined, { type: 'INVALID', data: null });
       expect(newState.length).toBe(0);
     });
 
     it('retrieved action should set posts to the data delivered', () => {
-      var newState = postsReducer(null, postDataRetrieved(posts));
+      const newState = postsReducer(null, postDataRetrieved(posts));
       expect(newState.length).toEqual(posts.length);
     });
   });
 
   describe('usersReducer', () => {
     it('should set users to empty for an unsupported action', () => {
-      var newState = usersReducer(undefined, { type: 'INVALID', data: null });
+      const newState = usersReducer(undefined, { type: 'INVALID', data: null });
       expect(newState.length).toEqual(0);
     });
 
     it('valid action should set the users to the data delivered', () => {
-      var newState = usersReducer(null, usersDataRetrieved(users));
+      const newState = usersReducer(null, usersDataRetrieved(users));
       expect(newState.length).toEqual(users.length);
     });
   });
 
   describe('filterReducer', () => {
     it('should default to zero for unsupported actions', () => {
-      var newState = filterReducer(undefined, { type: 'INVALID', data: null });
+      const newState = filterReducer(undefined, { type: 'INVALID', data: null });
       expect(newState).toEqual(0);
     });
 
     it('valid action should set the data to that passed in', () => {
-      var newState = filterReducer(null, userFilterChange(3));
+      const newState = filterReducer(null, userFilterChange(3));
       expect(newState).toEqual(3);
     });
   });
 
   describe('uiState reducer', () => {
     it('should default to \'loading\' for unsupported actions', () => {
-      var newState = uiReducer(undefined, { type: 'INVALID' });
+      const newState = uiReducer(undefined, { type: 'INVALID' });
       expect(newState).toEqual(UI_LOADING);
     });
 
     it('should default to \'loading\' for start retrieving action', () => {
-      var newState = uiReducer(undefined, startRetrievingData());
+      const newState = uiReducer(undefined, startRetrievingData());
       expect(newState).toEqual(UI_LOADING);
     });
 
     it('should default to \'loaded\' for posts retrieved action', () => {
-      var newState = uiReducer(undefined, postDataRetrieved(posts));
+      const newState = uiReducer(undefined, postDataRetrieved(posts));
       expect(newState).toEqual(UI_LOADED);
     });
   });
 
   describe('Immutable state', () => {
     it('should not mutate the state when setting posts', () => {
-      var initialState = {
+      const initialState = {
         posts: [],
         users: [],
         userFilter: 0
@@ -90,7 +90,7 @@ describe('Reducer tests', () => {
     });
 
     it('should not mutate the state when setting users', () => {
-      var initialState = {
+      const initialState = {
         posts: [],
         users: [],
         userFilter: 0
@@ -103,7 +103,7 @@ describe('Reducer tests', () => {
     });
 
     it('should not mutate the state when setting userFilter', () => {
-      var initialState = {
+      const initialState = {
         posts: [],
         users: [],
         userFilter: 0
