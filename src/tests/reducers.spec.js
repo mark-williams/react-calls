c50afd9197041eb95cf7bb286df159e2a4314d6b
mark-williams <markwill@hotmail.co.uk>
2018-02-15 09:05:33 +0000

    refactor: removed chai for test asserts as now doing this entirely within jest

--- src/tests/reducers.spec.js
+++ src/tests/reducers.spec.js
@@ -4,9 +4,6 @@ import { startRetrievingData, postDataRetrieved, usersDataRetrieved, userFilterC
 import deepFreeze from 'deep-freeze';
 
 /* eslint no-undefined: 0 */
-var chai = require('chai');
-var expect = chai.expect;
-
 describe('Reducer tests', () => {
   let posts = [];
   let users = [];
@@ -28,53 +25,53 @@ describe('Reducer tests', () => {
   describe('postsReducer', () => {
     it('should set posts to empty for an unsuported action', () => {
       var newState = postsReducer(undefined, { type: 'INVALID', data: null });
-      expect(newState).to.have.length(0);
+      expect(newState.length).toBe(0);
     });
 
     it('retrieved action should set posts to the data delivered', () => {
       var newState = postsReducer(null, postDataRetrieved(posts));
-      expect(newState).to.have.length(posts.length);
+      expect(newState.length).toEqual(posts.length);
     });
   });
 
   describe('usersReducer', () => {
     it('should set users to empty for an unsupported action', () => {
       var newState = usersReducer(undefined, { type: 'INVALID', data: null });
-      expect(newState).to.have.length(0);
+      expect(newState.length).toEqual(0);
     });
 
     it('valid action should set the users to the data delivered', () => {
       var newState = usersReducer(null, usersDataRetrieved(users));
-      expect(newState).to.have.length(users.length);
+      expect(newState.length).toEqual(users.length);
     });
   });
 
   describe('filterReducer', () => {
     it('should default to zero for unsupported actions', () => {
       var newState = filterReducer(undefined, { type: 'INVALID', data: null });
-      expect(newState).to.equal(0);
+      expect(newState).toEqual(0);
     });
 
     it('valid action should set the data to that passed in', () => {
       var newState = filterReducer(null, userFilterChange(3));
-      expect(newState).to.equal(3);
+      expect(newState).toEqual(3);
     });
   });
 
   describe('uiState reducer', () => {
     it('should default to \'loading\' for unsupported actions', () => {
       var newState = uiReducer(undefined, { type: 'INVALID' });
-      expect(newState).to.equal(UI_LOADING);
+      expect(newState).toEqual(UI_LOADING);
     });
 
     it('should default to \'loading\' for start retrieving action', () => {
       var newState = uiReducer(undefined, startRetrievingData());
-      expect(newState).to.equal(UI_LOADING);
+      expect(newState).toEqual(UI_LOADING);
     });
 
     it('should default to \'loaded\' for posts retrieved action', () => {
       var newState = uiReducer(undefined, postDataRetrieved(posts));
-      expect(newState).to.equal(UI_LOADED);
+      expect(newState).toEqual(UI_LOADED);
     });
   });
 
@@ -89,7 +86,7 @@ describe('Reducer tests', () => {
       deepFreeze(initialState);
 
       let newState = reducer(initialState, postDataRetrieved(posts));
-      expect(newState.posts).to.have.length(3);
+      expect(newState.posts.length).toEqual(3);
     });
 
     it('should not mutate the state when setting users', () => {
@@ -102,7 +99,7 @@ describe('Reducer tests', () => {
       deepFreeze(initialState);
 
       let newState = reducer(initialState, usersDataRetrieved(users));
-      expect(newState.users).to.have.length(3);
+      expect(newState.users.length).toEqual(3);
     });
 
     it('should not mutate the state when setting userFilter', () => {
@@ -115,7 +112,7 @@ describe('Reducer tests', () => {
       deepFreeze(initialState);
 
       let newState = reducer(initialState, userFilterChange(3));
-      expect(newState.userFilter).to.equal(3);
+      expect(newState.userFilter).toEqual(3);
     });
   });
 });
