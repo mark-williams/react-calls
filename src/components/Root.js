788cebaadb35a0ec09dcb7c8b1400e079f413885
mark-williams <markwill@hotmail.co.uk>
2018-02-20 09:38:32 +0000

    refactor: use Fragment

--- src/components/Root.js
+++ src/components/Root.js
@@ -1,16 +1,16 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 import { Provider } from 'react-redux';
 import PostList from './PostList';
 import store from '../store';
 
 const Root = () => (
   <Provider store={store}>
-    <div>
+    <Fragment>
       <h1>User Posts</h1>
       <div>
         <PostList />
       </div>
-    </div>
+    </Fragment>
   </Provider>
 );
 
